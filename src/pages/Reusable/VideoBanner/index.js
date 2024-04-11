ab518f62c9430df7c30e6ad375a7884a9050dfc1
Nidhi Bhatia <nidhibhatia@Nidhis-MacBook-Air.local>
2024-04-11 15:32:57 +0530

    downgrade the next version

--- src/pages/Reusable/VideoBanner/index.js
+++ src/pages/Reusable/VideoBanner/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import style from "../../index.module.scss"
+import style from "./VideoCss.module.scss"
 import { Typography } from '@mui/material';
-
 
 const VideoBanner = () => {
     return (
         <div className={`${style.banner_video}`}>
             <div className={style.video_div} />
-            <video playsinline="playsinline" autoPlay="autoplay" muted="muted" loop="loop" className={style.video_banner}>
+            <video playsinline="playsinline" autoPlay="autoPlay" muted="muted" loop="loop" className={style.video_banner}>
                 <source src="https://player.vimeo.com/external/406227029.sd.mp4?s=9f5369cd52db12305a27c05935da15b13ab84f45&profile_id=164&oauth2_token_id=57447761" type="video/mp4" />
             </video>
             <div className={style.video_text}>
-                <Typography className={`${style.heading}`}>The Unplanned Trip</Typography>
+                <Typography className={style.heading}> The Unplanned Trip</Typography>
                 <Typography className={style.subHeading}>Marvelous Uttarakhand</Typography>
             </div>
         </div>
